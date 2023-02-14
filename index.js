754a95eed889bc55297293bef79f37ff669ab91b
Vasily Kuzin <exer7um@gmail.com>
2023-02-14 17:13:03 +0300

    feat: Настройка конфигурации (#10)
    * Добавление конфигурации #3
    
    * Добавление в package.json #3

---
+++ index.js
@@ -0,0 +1,49 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:typescript-sort-keys/recommended",
+    "plugin:jsdoc/recommended",
+    "@nuxt/eslint-config",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: [
+    "eslint-plugin-tsdoc",
+    "sort-keys-fix",
+    "typescript-sort-keys",
+    "jsdoc",
+    "vue",
+  ],
+  root: true,
+  rules: {
+    "jsdoc/check-indentation": "warn",
+    "jsdoc/require-param-type": "off",
+    "jsdoc/require-returns-type": "off",
+    "jsdoc/sort-tags": "warn",
+    "sort-keys-fix/sort-keys-fix": "error",
+    "tsdoc/syntax": "error",
+    "vue/attributes-order": [
+      "error",
+      {
+        alphabetical: true,
+      },
+    ],
+    "vue/multi-word-component-names": "off",
+    "vue/require-prop-comment": [
+      "error",
+      {
+        type: "JSDoc",
+      },
+    ],
+  },
+}
